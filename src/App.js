8f7926b47286d2e1f0faaad6cf009ed589fce471
Rodrigo Sebastião <rodrigodanielss@gmail.com>
2020-01-11 19:34:53 -0300

    final

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Character extends React.Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      data: []
+    }
+  }
+
+  componentWillMount() {
+    this.getCharacter();
+  }  
+
+  passResult (data){
+    console.log("?", data);
+  }
+
+  async getCharacter(){   
+    await fetch('https://rickandmortyapi.com/api/character/')
+    .then((response) => { return response.json() })
+    .then((data) => {
+      console.log("data", data);
+      this.setState({data: data})
+    })
+  }
+
+  render(){
+    const {data} = this.state;
+    const character = data.results;  
+    if(!character){
+      return <div className="loading" style={{position: "absolute", top: "30%", left: "0", right: 0, textAlign: "center"}}>
+        "...Loading"</div>
+    }
+    return(
+      <section className="Character-Board">
+        <h1>Ricky and Morty Character Board</h1>
+          <div className="Character-Board-Wrap">
+          { character.map(function(char, index){
+              return  <div className="character-card" key={char.id} >
+                          <img src={char.image} />
+                          <div className="text">
+                            <p><strong> {char.name} </strong></p>
+                            <p> {char.species} </p>
+                            <p> {char.status} </p>
+                          </div>
+                      </div>
+            })}
+          </div>
+      </section>
+    )
+  }
 }
 
-export default App;
+export default Character;
